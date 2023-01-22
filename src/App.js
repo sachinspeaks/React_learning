5e8c209c4234c90bebaa1db1aeb489d9afd174a7
Sachin <sachingaud78@gmail.com>
2023-01-22 12:46:13 +0530

    Modified Textutils.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 // import logo from './logo.svg';
 // import './App.css';
-
 import Navbar from "./Components/Navbar";
 import Textform from "./Components/Textform";
+import About from "./Components/about";
+import React, {useState} from 'react';
+
 function App() {
+  const [mode, setmode] = useState('light');
+
+  const togglemode=()=>
+  {
+      if(mode==='light')setmode('dark');
+      else setmode('light');
+  };
+
   return (
     <>
-      <Navbar title="Mywebsite" abouttext="About US" />
+      <Navbar title="Mywebsite" abouttext="About US" mode={mode} togglemode={togglemode}/>
       <div className="container my-3">
       <Textform heading="Enter text : "/>
+      <About/>
       </div>
     </>
   );
--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,23 +1,79 @@
 import React,{useState} from 'react'
 
 export default function Textform(props) {
-    const [text,settext]=useState("Enter your text :");
+    const [text,settext]=useState("");
     const handleclick=()=>
     {
-        let newtext=text.toLocaleUpperCase();
+        let newtext=text.toUpperCase();
         settext(newtext);
+    };
+    const handleloclick=()=>
+    {
+        let newtext=text.toLowerCase();
+        settext(newtext);
+    };
+    const handlefield=()=>{
+        settext("");
     };
     const handlechange=(event)=>
     {
         settext(event.target.value);
     };
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text); 
+        props.showAlert("Copied to Clipboard!", "success");
+    };
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        settext(newText.join(" "));
+        props.showAlert("Extra spaces removed!", "success");
+    }
+    const handleicase=()=>
+    {
+        let ctext="";
+        for(let i=0;i<text.length;i++)
+        {
+            if(text[i]===text[i].toLowerCase())
+            {
+                ctext+=text[i].toUpperCase();
+            }
+            else
+            {
+                ctext+=text[i].toLowerCase();
+            }
+        }
+        settext(ctext);
+    };
     return (
-        <div>
-            <h2>{props.heading}</h2>
+        <>
+        <div className='container'>
+            <h2 className='my-3'>{props.heading}</h2>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handlechange} id="exampleFormControlTextarea1" rows="9"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleclick}>Convert to Uppercase</button>
+            <button type="button" className="btn btn-dark mx-2" onClick={handleclick}>Convert to Uppercase</button>
+            <button type="button" className="btn btn-dark mx-2" onClick={handleloclick}>Convert to Lowercase</button>
+            <button type="button" className="btn btn-dark mx-2" onClick={handlefield}>Clear Field</button>
+            <button type="button" className="btn btn-dark mx-2" onClick={handleicase}>Inverse Case</button>
+            <button type="button" className="btn btn-dark mx-2" onClick={handleCopy}>Copy to Clipboard</button>
+            <button type="button" className="btn btn-dark mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
+        <div className="container my-3">
+            <h2>
+                Your Word Summary :
+            </h2>
+            <p>
+                {text===""?0:text.split(" ").length} <b>Words</b><br/>
+                {text.length} <b>Characters</b> <br/>
+                <b>Time to read : </b>{text===""?0:text.split(" ").length*0.008} <b>Minutes</b><br/>
+                <h2 className='my-3'>
+                    Preview :
+                </h2>
+                <p>
+                    {text}
+                </p>
+            </p>
+        </div>
+        </>
     )
 }
